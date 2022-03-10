f63ca68fa929663f62ef23a4f2d132698df8e786
pennuel1 <makiapj@gmail.com>
2022-03-10 03:15:06 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,9 +1,9 @@
 module "s3-bucket" {
   source  = "terraform-aws-modules/s3-bucket/aws"
   version = "2.8.0"
-   bucket_prefix = var.prefix
-   bucket = "my-s3-bucket"
-   acl    = "private"
+  bucket_prefix = var.prefix
+  bucket = var.bucket_name
+  acl    = var.bucket_acl
 
    versioning = {
     enabled = true
--- variables.tf
+++ variables.tf
@@ -48,3 +48,11 @@ variable "placeholder" {
   default     = "placekitten.com"
   description = "Image-as-a-service URL. Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
 }
+
+variable "bucket_name" {
+  default = "my-s3-bucket2225"
+}
+
+variable "bucket_acl" {
+  default = "private"
+}

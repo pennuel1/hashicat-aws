c4b3bb383ae446bdbd9d3ddd2155ac55b99f53e0
pennuel1 <makiapj@gmail.com>
2022-03-10 04:41:50 +0000

    Added s3 bucket module

---
+++ s3-bucket.tf
@@ -0,0 +1,10 @@
+module "s3-bucket" {
+  source  = "app.terraform.io/pennuel123/s3-bucket/aws"
+  version = "2.8.0"
+  acceleration_status = "enabled"
+  bucket = "pennuelmakia"
+  bucket_prefix = var.prefix
+  policy = "random policy"
+  request_payer = "pennuel123"
+
+}

5315fa43209144e2fa2bf0d1836a91d374f4a481
pennuel1 <makiapj@gmail.com>
2022-03-10 04:15:52 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,6 +1,8 @@
 module "s3-bucket" {
-  source  = "git@github.com:terraform-aws-modules/terraform-aws-s3-bucket.git"
+  source  = "app.terraform.io/pennuel123/s3-bucket/aws"
   version = "2.8.0"
+  bucket_prefix = var.prefix
   bucket        = var.bucket_name
   acl           = var.bucket_acl
+ 
 }
